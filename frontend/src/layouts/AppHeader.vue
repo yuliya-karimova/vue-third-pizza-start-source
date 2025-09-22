f35f4c76a0b688c1fe68ce10a3399cc3e0159eef
Yuliya Karimova <ykarimovao@ozon.ru>
2025-09-22 23:30:28 +0300

    feat: create AppHeader, AppLayout and HomeView

---
+++ frontend/src/layouts/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header">
+    <div class="header__logo">
+      <a href="/" class="logo">
+        <img :src="logo" alt="V!U!E! Pizza logo" width="90" height="40" />
+      </a>
+    </div>
+    <div class="header__cart">
+      <a href="/cart">0 ₽</a>
+    </div>
+    <div class="header__user">
+      <a href="#" class="header__login"><span>Войти</span></a>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import logo from "@/assets/img/logo.svg";
+</script>
